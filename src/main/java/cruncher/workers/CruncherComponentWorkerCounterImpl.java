d1c79411ed8a56507baf5dfb0375892a366f154f
Luka Petrovic <petrovic.luka99@gmail.com>
2022-04-04 21:02:36 +0200

    Implemented CruncherComponentWorkerCounterImpl to correctly calculate the result

--- src/main/java/cruncher/workers/CruncherComponentWorkerCounterImpl.java
+++ src/main/java/cruncher/workers/CruncherComponentWorkerCounterImpl.java
@@ -37,38 +37,52 @@ public class CruncherComponentWorkerCounterImpl extends CruncherComponentWorker 
         Map<String, Long> results = new ConcurrentHashMap<>();
 
         try {
-            if (arity == 1) {
-                List<String> words = getWords(data);
-                for (final String word : words) {
+            StringBuilder currWord = new StringBuilder();
+            int spaces = 0;
+            int k = 0;
+            for (int i = 0; i < data.length(); i++) {
+                char currChar = data.charAt(i);
+                if (currChar == ' ') {
+                    spaces += 1;
+
+                    if (spaces == 1)
+                        k = i;
+
+                    if (spaces == arity) {
+                        String word = currWord.toString();
+                        if (results.containsKey(word))
+                            results.put(word, results.get(word) + 1);
+                        else
+                            results.put(word, 1L);
+
+                        spaces = 0;
+                        i = k;
+                        currWord.setLength(0);
+                    } else {
+                        currWord.append(" ");
+                    }
+                } else if (i == data.length() - 1) {
+                    currWord.append(currChar);
+                    String word = currWord.toString();
                     if (results.containsKey(word))
                         results.put(word, results.get(word) + 1);
                     else
                         results.put(word, 1L);
+
+                    spaces = 0;
+                    currWord.setLength(0);
+                }
+                else {
+                    currWord.append(currChar);
                 }
             }
 
-        } catch (OutOfMemoryError e) {
+        } catch (Exception e) {
             PipelineManager.getInstance().terminateApplication();
         }
 
         filesInCrunchingProcess.remove(getFileInputResult().getFilename());
 
         return results;
-    }
-
-    private List<String> getWords(String text) {
-        List<String> words = new ArrayList<>();
-        BreakIterator breakIterator = BreakIterator.getWordInstance();
-        breakIterator.setText(text);
-        int lastIndex = breakIterator.first();
-        while (BreakIterator.DONE != lastIndex) {
-            int firstIndex = lastIndex;
-            lastIndex = breakIterator.next();
-            if (lastIndex != BreakIterator.DONE && Character.isLetterOrDigit(text.charAt(firstIndex))) {
-                words.add(text.substring(firstIndex, lastIndex));
-            }
-        }
-
-        return words;
     }
 }
